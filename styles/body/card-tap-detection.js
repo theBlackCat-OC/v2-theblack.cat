f5077a31a69ea96953279d7e12b862f2b43ac7b5
the Black Cat <github@mail.theblack.cat>
2023-11-10 02:30:27 +0100

    Tap target

--- styles/body/card-tap-detection.js
+++ styles/body/card-tap-detection.js
@@ -1,8 +1,27 @@
-// Get the element
-var tapElement = document.getElementById('tapElement');
-// Add a touchstart event listener
-tapElement.addEventListener('touchstart', function() {
-    // Apply the CSS style when tapped
-        tapElement.classList.add('tapped');
-        // You can add additional actions or styles here
+var tapOCCommWorks = document.getElementById('oc-commworks');
+var tapBodyReferences = document.getElementById('oc-body-references');
+var tapTouCard = document.getElementById('tou-card');
+var tapWcifCard = document.getElementById('wcif-card');
+
+document.addEventListener('touchstart', function (event) {
+    if (tapOCCommWorks.contains(event.target)) {
+        tapOCCommWorks.classList.add('card-wrapper-hover');
+    } else {
+        tapOCCommWorks.classList.remove('card-wrapper-hover');
+    }
+    if (tapBodyReferences.contains(event.target)) {
+        tapBodyReferences.classList.add('card-wrapper-hover');
+    } else {
+        tapBodyReferences.classList.remove('card-wrapper-hover');
+    }
+    if (tapTouCard.contains(event.target)) {
+        tapTouCard.classList.add('card-wrapper-hover');
+    } else {
+        tapTouCard.classList.remove('card-wrapper-hover');
+    }
+    if (tapWcifCard.contains(event.target)) {
+        tapWcifCard.classList.add('card-wrapper-hover');
+    } else {
+        tapWcifCard.classList.remove('card-wrapper-hover');
+    }
 });
