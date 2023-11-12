4d076e7f7d66603057fcf8ab0c51a6235fb64b92
the Black Cat <github@mail.theblack.cat>
2023-11-12 04:03:02 +0100

    BG changes

--- scripts/button-activations/cards-buttons.js
+++ scripts/button-activations/cards-buttons.js
@@ -2,5 +2,5 @@ document.getElementById('oc-body-references').onclick = function() {
     window.open('https://reference.theblack.cat', '_blank');
 };
 document.getElementById('oc-commworks').onclick = function() {
-    window.location.href = 'https://theblack.cat/commissioned-works';
+    window.open('https://theblack.cat/commissioned-works', '_blank');
 };
--- styles/body/gallery-cards/commworks-bg.js
+++ styles/body/gallery-cards/commworks-bg.js
@@ -3,6 +3,7 @@ const imageUrls = [
     'resources/images/bg-images/horizontal/chaosringen-color-horizontal.avif',
     'resources/images/bg-images/horizontal/mijeh-aug2023-horizontal.avif',
     'resources/images/bg-images/horizontal/ayaben-oct2023-fullcolor.avif',
+    'resources/images/bg-images/horizontal/hairyhyur-gpose-cardbg.avif',
   ];
   function getRandomImage() {
     return imageUrls[Math.floor(Math.random() * imageUrls.length)];
