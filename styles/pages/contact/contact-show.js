2f7513ac0cf5894d555472bf4271fa4e69327969
the Black Cat <github@mail.theblack.cat>
2023-11-13 15:50:48 +0100

    Revert changes

--- styles/pages/contact/contact-show.js
+++ styles/pages/contact/contact-show.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     var contactPageWrapper = document.getElementById('contact-page-wrapper');
 
     function showContactPage() {
-        contactPageWrapper.style.visibility = '';
+        contactPageWrapper.style.display = '';
     }
 
     headerContact.addEventListener('click', showContactPage);
