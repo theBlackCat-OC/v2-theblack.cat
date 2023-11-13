ac9e68e3b3253dad1a8e142ec205b2875e0cfecd
the Black Cat <github@mail.theblack.cat>
2023-11-14 00:29:28 +0100

    Fonts, 🔞

--- styles/pages/contact/contact-hide-esc.js
+++ styles/pages/contact/contact-hide-esc.js
@@ -10,4 +10,5 @@ document.addEventListener('DOMContentLoaded', function () {
             hideContactPage();
         }
     });
+    contactPageWrapper.addEventListener('click', hideContactPage);
 });
